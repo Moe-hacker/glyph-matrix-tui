0cc3880a2fef2cdd99014854c7573b289f0f53bb
Moe-hacker <moe-hacker@outlook.com>
2025-12-03 23:27:23 +0800

    up

--- glyph_matrix.c
+++ glyph_matrix.c
@@ -47,8 +47,8 @@ void print_matrix(char **grid)
 	fflush(stdout);
 	struct winsize w;
 	ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
-	int x_offset = (w.ws_col - SIZE * 2) / 2;
-	int y_offset = (w.ws_row - SIZE) / 2;
+	int x_offset = (w.ws_col - SIZE * 2) / 2 + 1;
+	int y_offset = (w.ws_row - SIZE) / 2 + 1;
 	for (int i = 0; i < y_offset; i++) {
 		printf("\n");
 		fflush(stdout);
